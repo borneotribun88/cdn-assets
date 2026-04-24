748c8677ff58e591126e6f752bea35cb55a3dfd3
borneotribun88 <borneotribunnews@gmail.com>
2026-04-25 04:42:13 +0700

    Update iklan-utama.js

--- iklan-utama.js
+++ iklan-utama.js
@@ -10,7 +10,7 @@ document.getElementById("iklan-box-utama").innerHTML = `
 </style>
 
 <img 
-src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiburS4cuSjGWpWgxeNxNKQVz4SEsvLbWm01OvbvinVw1LfwR1c7I519RgeORGgHcv1xywqmkTG3-tvREXJc12oxkX6h_6YC8ciDSZp7V8zU9CDUZKDn6M9HxiKMv6Kqdr_ezsFCJLKbq4427oKJFvtq4iL0bI3xS3uGt-ng4IsRgfkz8jExvZL8ct2jhqU/s16000/ucapan-dprd-sanggau-dandim-sanggau.webp" 
+src="URL-GAMBAR-KEDUA.webp" 
 alt="iklan"
 onerror="this.style.display='none';"
 >
